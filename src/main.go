24623e89425e1490182c317170f897411344194b
followedwind <followedwind@gmail.com>
2021-02-23 09:38:47 +0000

    first commit

---
+++ src/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
+
+func main() {
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"), slack.OptionDebug(true))
+	signingSecret := os.Getenv("SIGNING_SECRET")
+	// If you set debugging, it will log all requests to the console
+	// Useful when encountering issues
+	// slack.New("YOUR_TOKEN_HERE", slack.OptionDebug(true))
+
+	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("reached")
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if _, err := sv.Write(body); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := sv.Ensure(); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		fmt.Printf("%#v\n", eventsAPIEvent)
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+			}
+		}
+	})
+	fmt.Println("[INFO] Server listening")
+	http.ListenAndServe(":13000", nil)
+
+}
