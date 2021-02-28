63c96c21583c51f0130af6611116cf7eb379362b
followedwind <followedwind@gmail.com>
2021-02-28 06:02:28 +0000

    slack受け答え

---
+++ src/command_list.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/slack-go/slack"
+)
+
+func commandList() slack.MsgOption {
+
+	headerText := slack.NewTextBlockObject("mrkdwn", "お呼びでございましょうか\n*ご用件は？*", false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	lightOnBtnTxt := slack.NewTextBlockObject("plain_text", "点灯", false, false)
+	lightOnBtn := slack.NewButtonBlockElement("", "light:on", lightOnBtnTxt)
+
+	lightOffBtnTxt := slack.NewTextBlockObject("plain_text", "消灯", false, false)
+	lightOffBtn := slack.NewButtonBlockElement("", "light:off", lightOffBtnTxt)
+
+	lockBtnTxt := slack.NewTextBlockObject("plain_text", "施錠", false, false)
+	lockBtn := slack.NewButtonBlockElement("", "key:lock", lockBtnTxt)
+
+	unlockBtnTxt := slack.NewTextBlockObject("plain_text", "解錠", false, false)
+	unlockBtn := slack.NewButtonBlockElement("", "key:unlock", unlockBtnTxt)
+
+	actionBlock := slack.NewActionBlock("", lightOnBtn, lightOffBtn, lockBtn, unlockBtn)
+
+	msg := slack.MsgOptionBlocks(
+		headerSection,
+		actionBlock,
+	)
+	return msg
+}
--- src/main.go
+++ src/main.go
@@ -6,7 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
@@ -19,7 +23,6 @@ func main() {
 	// slack.New("YOUR_TOKEN_HERE", slack.OptionDebug(true))
 
 	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("reached")
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -54,13 +57,52 @@ func main() {
 			w.Header().Set("Content-Type", "text")
 			w.Write([]byte(r.Challenge))
 		}
-		fmt.Printf("%#v\n", eventsAPIEvent)
+		//fmt.Printf("%#v\n", eventsAPIEvent)
+		//fmt.Printf("%#v\n", eventsAPIEvent.Data)
+		fmt.Printf("%#v\n", eventsAPIEvent.InnerEvent.Data)
 		if eventsAPIEvent.Type == slackevents.CallbackEvent {
 			innerEvent := eventsAPIEvent.InnerEvent
 			switch ev := innerEvent.Data.(type) {
 			case *slackevents.AppMentionEvent:
-				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+				//api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+				api.PostMessage(ev.Channel, commandList())
 			}
+		}
+	})
+	http.HandleFunc("/interactive", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("interactive")
+		var payload slack.InteractionCallback
+		err := json.Unmarshal([]byte(r.FormValue("payload")), &payload)
+		if err != nil {
+			fmt.Printf("Could not parse action response JSON: %v", err)
+			//ここでエラーだとChannelの取得もできない
+			//api.PostMessage(payload.Channel, slack.MsgOptionText(fmt.Printf("指令の解析に失敗しました: %v", err), false))
+			return
+		}
+		channelId := payload.Channel.GroupConversation.Conversation.ID
+		fmt.Printf("%#v\n", payload.ActionCallback.BlockActions[0].Value)
+		api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("%sを受け付けました", payload.ActionCallback.BlockActions[0].Text.Text), false))
+		// TODO move lambda to us-west-2
+		sess, _ := session.NewSessionWithOptions(session.Options{
+			Profile: "default",
+			Config: aws.Config{
+				Region: aws.String("us-west-2"),
+			},
+		})
+		svc := lambda.New(sess)
+		command := payload.ActionCallback.BlockActions[0].Value
+		commands := strings.Split(command, ":")
+		input := &lambda.InvokeInput{
+			FunctionName: aws.String("home-iot-invoker"),
+			Payload:      []byte(fmt.Sprintf("{\"target\":\"%s\", \"command\":\"%s\"}", commands[0], commands[1])),
+			//Qualifier:    aws.String("1"),
+		}
+		if resp, err := svc.Invoke(input); err != nil {
+			api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("処置に失敗しました: %v", err), false))
+		} else if *resp.StatusCode != int64(200) {
+			api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("処置に失敗しました: %v", resp), false))
+		} else {
+			api.PostMessage(channelId, slack.MsgOptionText("処置しました", false))
 		}
 	})
 	fmt.Println("[INFO] Server listening")
