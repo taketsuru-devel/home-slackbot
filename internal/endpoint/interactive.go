66f21b1eda47753a9334628048780d2d2715e003
followedwind <followedwind@gmail.com>
2021-04-18 07:02:14 +0000

    home-iot切り出し

--- internal/endpoint/interactive.go
+++ internal/endpoint/interactive.go
@@ -3,9 +3,6 @@ package endpoint
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/followedwind/slackbot/internal/util"
 	"github.com/slack-go/slack"
 	"net/http"
@@ -15,7 +12,7 @@ import (
 type InteractiveEndpoint struct{}
 
 func (i *InteractiveEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	api := util.GetSlackClient()
+	//情報取得
 	var payload slack.InteractionCallback
 	err := json.Unmarshal([]byte(r.FormValue("payload")), &payload)
 	if err != nil {
@@ -24,28 +21,20 @@ func (i *InteractiveEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	channelId := payload.Channel.GroupConversation.Conversation.ID
-	util.DebugLog(fmt.Sprintf("%#v", payload.ActionCallback.BlockActions[0].Value))
-	api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("%sを受け付けました", payload.ActionCallback.BlockActions[0].Text.Text), false))
-	sess, _ := session.NewSessionWithOptions(session.Options{
-		//Profile; "default",
-		Config: aws.Config{
-			Region:                        aws.String("us-west-2"),
-			CredentialsChainVerboseErrors: aws.Bool(true),
-		},
-	})
-	svc := lambda.New(sess)
 	command := payload.ActionCallback.BlockActions[0].Value
+	commandDisp := payload.ActionCallback.BlockActions[0].Text.Text
+
+	util.DebugLog(fmt.Sprintf("command:%#v", payload.ActionCallback.BlockActions[0].Value))
+
+	//とりあえず返事
+	api := util.GetSlackClient()
+	api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("%sを受け付けました", commandDisp), false))
+
+	//モノに指令
 	commands := strings.Split(command, ":")
-	input := &lambda.InvokeInput{
-		FunctionName: aws.String("home-iot-invoker"),
-		Payload:      []byte(fmt.Sprintf("{\"target\":\"%s\", \"command\":\"%s\"}", commands[0], commands[1])),
-		//Qualifier:    aws.String("1"),
+	responseText := "処置しました"
+	if err := util.IotInvoke(commands[0], commands[1]); err != nil {
+		responseText = fmt.Sprintf("処置に失敗しました: %v", err)
 	}
-	if resp, err := svc.Invoke(input); err != nil {
-		api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("処置に失敗しました: %v", err), false))
-	} else if *resp.StatusCode != int64(200) {
-		api.PostMessage(channelId, slack.MsgOptionText(fmt.Sprintf("処置に失敗しました: %v", resp), false))
-	} else {
-		api.PostMessage(channelId, slack.MsgOptionText("処置しました", false))
-	}
+	api.PostMessage(channelId, slack.MsgOptionText(responseText, false))
 }
